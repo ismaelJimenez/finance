0724f28e1216d984d1a6324bd2197cfd40abc9e7
argos <levantedevelopments@gmail.com>
2019-09-01 21:02:56 +0200

    added dart dev to travis

--- example/main.dart
+++ example/main.dart
@@ -5,39 +5,39 @@ Future<void> main(List<String> arguments) async {
       await Finance.downloadRawQuote(
           QuoteProvider.yahoo, <String>['KO', 'GOOG']);
 
-  print('Number of quotes retrieved : ${quoteRaw.keys.length}.');
+  print('Number of quotes retrieved: ${quoteRaw.keys.length}.');
   print(
-      'Number of attributes retrieved for KO : ${quoteRaw['KO'].keys.length}.');
+      'Number of attributes retrieved for KO: ${quoteRaw['KO'].keys.length}.');
   print(
-      'Current market price for KO : ${quoteRaw['GOOG']['regularMarketPrice']}.');
+      'Current market price for KO: ${quoteRaw['GOOG']['regularMarketPrice']}.');
   print(
-      'Number of attributes retrieved for GOOG : ${quoteRaw['GOOG'].keys.length}.');
+      'Number of attributes retrieved for GOOG: ${quoteRaw['GOOG'].keys.length}.');
   print(
-      'Current market price for KO : ${quoteRaw['GOOG']['regularMarketPrice']}.');
+      'Current market price for KO: ${quoteRaw['GOOG']['regularMarketPrice']}.');
 
   final Map<String, Map<String, dynamic>> quotePrice =
       await Finance.downloadQuotePrice(
           QuoteProvider.yahoo, <String>['KO', 'GOOG']);
 
-  print('Number of quotes retrieved : ${quotePrice.keys.length}.');
+  print('Number of quotes retrieved: ${quotePrice.keys.length}.');
   print(
-      'Number of attributes retrieved for KO : ${quotePrice['KO'].keys.length}.');
-  print('Current market price for KO : ${quotePrice['KO']['price']}.');
+      'Number of attributes retrieved for KO: ${quotePrice['KO'].keys.length}.');
+  print('Current market price for KO: ${quotePrice['KO']['price']}.');
   print(
-      'Number of attributes retrieved for GOOG : ${quotePrice['GOOG'].keys.length}.');
-  print('Current market price for KO : ${quotePrice['GOOG']['price']}.');
+      'Number of attributes retrieved for GOOG: ${quotePrice['GOOG'].keys.length}.');
+  print('Current market price for KO: ${quotePrice['GOOG']['price']}.');
 
   final Map<String, Map<String, dynamic>> cryptoQuoteRaw =
       await Finance.downloadRawQuote(
           QuoteProvider.coincap, <String>['bitcoin', 'ethereum']);
 
-  print('Number of quotes retrieved : ${cryptoQuoteRaw.keys.length}.');
+  print('Number of quotes retrieved: ${cryptoQuoteRaw.keys.length}.');
   print(
-      'Number of attributes retrieved for bitcoin : ${cryptoQuoteRaw['bitcoin'].keys.length}.');
+      'Number of attributes retrieved for bitcoin: ${cryptoQuoteRaw['bitcoin'].keys.length}.');
   print(
-      'Current market price for bitcoin : ${cryptoQuoteRaw['bitcoin']['priceUsd']}.');
+      'Current market price for bitcoin: ${cryptoQuoteRaw['bitcoin']['priceUsd']}.');
   print(
-      'Number of attributes retrieved for ethereum : ${cryptoQuoteRaw['ethereum'].keys.length}.');
+      'Number of attributes retrieved for ethereum: ${cryptoQuoteRaw['ethereum'].keys.length}.');
   print(
-      'Current market price for ethereum : ${cryptoQuoteRaw['ethereum']['priceUsd']}.');
+      'Current market price for ethereum: ${cryptoQuoteRaw['ethereum']['priceUsd']}.');
 }
